82a802c2af7ce5f5811f08ab0806559b20af22f7
mgmaroa <matikogmaroa@gmail.com>
2026-03-20 15:37:41 +0300

    creates the custom user model and updates settings.py to use the custom user model

--- examcardsite/exam_portal/models.py
+++ examcardsite/exam_portal/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('finance', 'Finance'),
+        ('registrar', 'Registrar'),
+        ('student', 'Student'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
